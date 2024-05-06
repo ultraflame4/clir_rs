b5882c6b740ccb8a0cb2c63720b6196486ddeb9f
ultr42 <ultr42@skiff.com>
2024-05-06 12:31:40 +0800

    calculate output size

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 use argh::FromArgs;
+use image::io::Reader as ImageReader;
 
 #[derive(FromArgs, Debug)]
 /// Renders an image to the console as unicode art
-struct Args {
+struct CliArgs {
     /// path to the source image
     #[argh(positional)]
     source: String,
-    
+
     /// saves output to this path.
     #[argh(option)]
     output: Option<String>,
 
     /// disables automatic resizing of output size to fit the terminal if available. Using --width or --height will override the detected values.
-    /// 
+    ///
     /// When not available or disabled, autosize sets width to 100, height is derived from aspect ratio . If --no_keep_aspect is set, height will be set to 100
     #[argh(switch)]
     no_autosize: bool,
 
-    /// specify width of the output.
-    #[argh(option, short='w')]
+    /// specify width of the output in number of chars.
+    #[argh(option, short = 'w')]
     width: Option<usize>,
 
-    /// specify height of the output.
-    #[argh(option, short='h')]
+    /// specify height of the output in number of rows.
+    #[argh(option, short = 'h')]
     height: Option<usize>,
 
     /// disables keeping of aspect ratio when resizing images. No effect when both --width & --height is used.
@@ -31,23 +32,74 @@ struct Args {
 
     /// disables colors, in rendered output. Result will be black & white
     #[argh(switch)]
-    no_color:bool,
+    no_color: bool,
 
     /// enables plain text mode, useful for rendering unicode art.
     #[argh(switch)]
-    plain_text:bool,
+    plain_text: bool,
 
     /// enable debug outputs, which will be stored in `./clir_rs_debug/`
     #[argh(switch)]
     debug: bool,
-    
+
+    /// overrides all size options. Uses the orginal image's size. Calculation is (image.width / CELL_W, image.height / CELL_H) Where CELL_W & CELL_H is typically 2 & 4 respectively.
+    #[argh(switch)]
+    use_original_image_size: bool,
 }
 
 const DEFAULT_WIDTH: usize = 100;
 const DEFAULT_HEIGHT: usize = 100;
 
-fn main() {
-    let args: Args = argh::from_env();
+enum RenderMode {
+    Color,
+    NoColor,
+    PlainText,
+}
 
-    println!("{:#?}",args)
+struct RenderSettings {
+    height: u32,
+    width: u32,
+    render_mode: RenderMode,
+    output: Option<String>,
+}
+impl RenderSettings {
+    pub fn scale_aspect(width: usize, aspect: f32) -> usize {
+        let rounded: u32 = (width as f32 / aspect).round() as u32;
+        rounded as usize
+    }
+
+    pub fn autodetected_size(aspect: f32) -> (usize, usize) {
+        match termsize::get() {
+            Some(size) => (size.cols as usize, size.rows as usize),
+            None => (DEFAULT_WIDTH, Self::scale_aspect(DEFAULT_WIDTH, aspect)),
+        }
+    }
+
+    pub fn from_args(args: CliArgs) {
+        let img = ImageReader::open("./test_resource/test_image.png")
+            .unwrap()
+            .decode()
+            .unwrap();
+
+        let aspect = img.width() as f32 / img.height() as f32;
+  
+        let output_size = if args.use_original_image_size {
+            (img.width() as usize, img.height() as usize)
+        } else {
+            let suggested_size = Self::autodetected_size(aspect);
+            (
+                args.width.unwrap_or(suggested_size.0),
+                args.height.unwrap_or(suggested_size.1),
+            )
+        };
+
+        
+
+    }
+}
+
+fn main() {
+    let args: CliArgs = argh::from_env();
+    // let settings = RenderSettings::from(args);
+    println!("{:#?}", args)
 }
