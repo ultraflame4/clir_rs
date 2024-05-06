71ca3d71fca5b221ef8ef54aae0bad0ba696f933
ultr42 <ultr42@skiff.com>
2024-05-06 12:05:58 +0800

    i think that is all the commands

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,17 @@ struct Args {
     #[argh(option)]
     output: Option<String>,
 
-    /// disables automatic resizing of output size to fit the terminal if available. Using --width or --height overrides the detected values.
+    /// disables automatic resizing of output size to fit the terminal if available. Using --width or --height will override the detected values.
+    /// 
+    /// When not available or disabled, autosize sets width to 100, height is derived from aspect ratio . If --no_keep_aspect is set, height will be set to 100
     #[argh(switch)]
     no_autosize: bool,
 
-    /// width of the output
+    /// specify width of the output.
     #[argh(option, short='w')]
     width: Option<usize>,
 
-    /// height of the output
+    /// specify height of the output.
     #[argh(option, short='h')]
     height: Option<usize>,
 
@@ -27,11 +29,22 @@ struct Args {
     #[argh(switch)]
     no_keep_aspect: bool,
 
+    /// disables colors, in rendered output. Result will be black & white
+    #[argh(switch)]
+    no_color:bool,
+
+    /// enables plain text mode, useful for rendering unicode art.
+    #[argh(switch)]
+    plain_text:bool,
+
     /// enable debug outputs, which will be stored in `./clir_rs_debug/`
     #[argh(switch)]
     debug: bool,
     
 }
+
+const DEFAULT_WIDTH: usize = 100;
+const DEFAULT_HEIGHT: usize = 100;
 
 fn main() {
     let args: Args = argh::from_env();
